a11bac9bf2dc72992b5b60c015d9c5edd765d9bf
JungminLee97 <jungmin7618@naver.com>
2025-02-04 15:13:12 +0900

    2일차진행중중

--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,6 +1,12 @@
-import PokemonList from "./PokemonList";
-const PokemonCard = () => {
-  return <div>{PokemonList}</div>;
+const PokemonCard = ({ pokemon }) => {
+  return (
+    <div>
+      <img src="{pokemon.img_url}" alt="{pokemon.korean_name}" />
+      <p>{pokemon.korean_name}</p>
+      <p>{pokemon.id}</p>
+      <p>{pokemon.types.join(", ")}</p>
+      <button>추가</button>
+    </div>
+  );
 };
-
 export default PokemonCard;
--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,66 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import MOCK_DATA from "../Components/MOCK_DATA";
+import PokemonList from "../Components/PokemonList";
+import { useState } from "react";
+
 const Dex = () => {
+  const [pokemons, setPokemons] = useState(MOCK_DATA);
   const navigate = useNavigate();
-  const NavBox = styled.div`
-    width: 1400px;
-    height: 300px;
-    border: 1px solid red;
-    margin: 30px auto 30px auto;
-    display: flex;
-    flex-direction: column;
-    background-color: #f0eeee;
-  `;
-  const MainBox = styled.div`
-    width: 1400px;
-    height: 300vh;
-    border: 1px solid red;
-    background-color: #f0eeee;
-    margin: 30px auto 30px auto;
-    display: flex;
-    overflow: auto;
-    flex-flow: row wrap;
-    justify-content: space-evenly;
-    align-content: flex-start;
-  `;
-  const NavBox1 = styled.div`
-    width: 1400px;
-    height: 100px;
-    text-align: center;
-    align-content: flex-end;
-    font-size: large;
-    color: red;
-    font-weight: 600;
-  `;
-  const NavBox2 = styled.div`
-    width: 1400px;
-    height: 200px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-  `;
-  const NavBox3 = styled.div`
-    width: 120px;
-    height: 120px;
-    border: 1px dashed black;
-    background-color: white;
-  `;
-  const MainBox1 = styled.div`
-    width: 210px;
-    height: 250px;
-    border-radius: 10px;
-    background-color: white;
-    display: flex;
-    flex-direction: column;
-    margin: 10px;
-    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
-  `;
-  //   const animals = {
-  //     name: "이상해씨",
-  //     age: "30",
-  //     address: "forest",
-  //   };
+
   return (
     <div>
       <NavBox>
@@ -75,7 +22,8 @@ const Dex = () => {
         </NavBox2>
       </NavBox>
       <MainBox>
-        <MainBox1 />
+        {/* <MainBox1 /> */}
+        <PokemonList pokemons={pokemons} />
       </MainBox>
       <button
         onClick={() => {
@@ -94,5 +42,60 @@ const Dex = () => {
     </div>
   );
 };
+
+const NavBox = styled.div`
+  width: 1400px;
+  height: 300px;
+  border: 1px solid red;
+  margin: 30px auto 30px auto;
+  display: flex;
+  flex-direction: column;
+  background-color: #f0eeee;
+`;
+const MainBox = styled.div`
+  width: 1400px;
+  height: 300vh;
+  border: 1px solid red;
+  background-color: #f0eeee;
+  margin: 30px auto 30px auto;
+  display: flex;
+  overflow: auto;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+  align-content: flex-start;
+`;
+const NavBox1 = styled.div`
+  width: 1400px;
+  height: 100px;
+  text-align: center;
+  align-content: flex-end;
+  font-size: large;
+  color: red;
+  font-weight: 600;
+`;
+const NavBox2 = styled.div`
+  width: 1400px;
+  height: 200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+const NavBox3 = styled.div`
+  width: 120px;
+  height: 120px;
+  border: 1px dashed black;
+  background-color: white;
+`;
+const MainBox1 = styled.div`
+  width: 210px;
+  height: 250px;
+  border-radius: 10px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+`;
 
 export default Dex;
